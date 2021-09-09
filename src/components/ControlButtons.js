7cacb779449a825a13cd41c5864c226e9e2f2cae
Michal Koptewicz <mkoptewicz@gmail.com>
2021-09-09 11:45:18 +0200

    add working buttons

--- src/components/ControlButtons.js
+++ src/components/ControlButtons.js
@@ -1,10 +1,35 @@
-import "./ControlButtons.css"
+import "./ControlButtons.css";
 
-const ControlButtons = () => {
+const ControlButtons = ({
+  isRunning,
+  isPaused,
+  onStart,
+  onStop,
+  onPause,
+  onContinue,
+}) => {
   return (
     <div className="control-buttons">
-      <button>Start</button>
-      <button>Pause</button>
+      {!isRunning && !isPaused && (
+        <button onClick={onStart} className="button button--wide">
+          Start
+        </button>
+      )}
+      {isRunning && (
+        <button onClick={onStop} className="button">
+          Stop
+        </button>
+      )}
+      {isRunning && (
+        <button onClick={onPause} className="button">
+          Pause
+        </button>
+      )}
+      {isPaused && (
+        <button onClick={onContinue} className="button button--wide">
+          Continue
+        </button>
+      )}
     </div>
   );
 };
--- src/components/pages/Timer.js
+++ src/components/pages/Timer.js
@@ -1,13 +1,57 @@
+import { useState, useEffect } from "react";
+
 import ProgressCircle from "../ProgressCircle";
 import ControlButtons from "../ControlButtons";
 import "./Timer.css";
 
 const Timer = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [elapsedTimeInSeconds, setElapsedTimeInSeconds] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    isRunning
+      ? (interval = setInterval(() => {
+          setElapsedTimeInSeconds(prevTime => prevTime + 0.1);
+        }, 100))
+      : clearInterval(interval);
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const startHandler = () => {
+    setIsRunning(true);
+  };
+  const stopHandler = () => {
+    setIsRunning(false);
+    setElapsedTimeInSeconds(0);
+  };
+  const pauseHandler = () => {
+    setIsPaused(true);
+    setIsRunning(false);
+  };
+  const continueHandler = () => {
+    setIsRunning(true);
+    setIsPaused(false);
+  };
+
+  console.log(elapsedTimeInSeconds);
+
   return (
     <>
       <h1>Timer</h1>
-      <ProgressCircle timeInSeconds={5 * 60} />
-      <ControlButtons />
+      <ProgressCircle
+        timeInSeconds={5 * 60}
+        elapsedTimeInSeconds={elapsedTimeInSeconds}
+      />
+      <ControlButtons
+        isRunning={isRunning}
+        isPaused={isPaused}
+        onStart={startHandler}
+        onStop={stopHandler}
+        onPause={pauseHandler}
+        onContinue={continueHandler}
+      />
     </>
   );
 };
