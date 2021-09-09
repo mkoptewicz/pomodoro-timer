92cefb7f8af63a631e97451cd75565ae15da6f60
Michal Koptewicz <mkoptewicz@gmail.com>
2021-09-09 10:13:28 +0200

    add routes and create dummy components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Layout from "./components/Layout";
+import Timer from "./components/pages/Timer";
+import Tasks from "./components/pages/Tasks";
+import Settings from "./components/pages/Settings";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
     <div className="app">
       <Layout />
-      <Switch></Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Timer />
+        </Route>
+        <Route path="/tasks">
+          <Tasks />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
     </div>
   );
 }
