6c932bea516dd6ae1893624a576713e1691879d3
Michal Koptewicz <mkoptewicz@gmail.com>
2021-09-09 10:46:43 +0200

    add basic ControlButtons component

---
+++ src/components/ControlButtons.js
@@ -0,0 +1,12 @@
+import "./ControlButtons.css"
+
+const ControlButtons = () => {
+  return (
+    <div className="control-buttons">
+      <button>Start</button>
+      <button>Pause</button>
+    </div>
+  );
+};
+
+export default ControlButtons;
--- src/components/pages/Timer.js
+++ src/components/pages/Timer.js
@@ -1,11 +1,13 @@
 import ProgressCircle from "../ProgressCircle";
+import ControlButtons from "../ControlButtons";
 import "./Timer.css";
 
 const Timer = () => {
   return (
     <>
       <h1>Timer</h1>
-      <ProgressCircle timeInSeconds={60} />
+      <ProgressCircle timeInSeconds={5 * 60} />
+      <ControlButtons />
     </>
   );
 };
