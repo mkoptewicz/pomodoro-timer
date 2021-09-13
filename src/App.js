08f9605e04de8b9f7dc0692c184a371143f43d26
Michal Koptewicz <mkoptewicz@gmail.com>
2021-09-13 09:06:53 +0200

    lift state up (timer works when other routes are active)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { useState, useEffect, useContext, useRef, useCallback } from "react";
+
 import { Switch, Route } from "react-router-dom";
+
+import SettingsContext from "./components/context/settings-contex";
+import completedSound from "./sounds/completed.mp3";
 
 import "./App.css";
 
@@ -9,12 +14,87 @@ import Settings from "./components/pages/Settings";
 import NotFound from "./components/pages/NotFound";
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [elapsedTimeInSeconds, setElapsedTimeInSeconds] = useState(0);
+
+  const settingsCtx = useContext(SettingsContext);
+  const { pomodoroMins, pomodoroSecs } = settingsCtx.settings;
+  const pomodoroTimeInSeconds = pomodoroMins * 60 + parseInt(pomodoroSecs);
+
+  // Run the timer every 0.1s
+  useEffect(() => {
+    let interval;
+    if (elapsedTimeInSeconds >= pomodoroTimeInSeconds) {
+      const audio = new Audio(completedSound);
+      audio.play();
+      setIsRunning(false);
+      setElapsedTimeInSeconds(0);
+    }
+    isRunning
+      ? (interval = setInterval(() => {
+          setElapsedTimeInSeconds(prevTime => prevTime + 0.1);
+        }, 100))
+      : clearInterval(interval);
+    return () => clearInterval(interval);
+  }, [elapsedTimeInSeconds, pomodoroTimeInSeconds, isRunning]);
+
+  //Run the timer when tab is inactive
+  const hideTime = useRef(0);
+
+  const visibilityChangeHandler = useCallback(() => {
+    if (document.hidden) {
+      hideTime.current = Date.now();
+    } else {
+      const elapsedTimeWhenHidden = (Date.now() - hideTime.current) / 1000;
+
+      setElapsedTimeInSeconds(
+        prevElapsedTime => prevElapsedTime + elapsedTimeWhenHidden
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isRunning) {
+      document.addEventListener("visibilitychange", visibilityChangeHandler);
+    }
+    return () => {
+      document.removeEventListener("visibilitychange", visibilityChangeHandler);
+    };
+  }, [isRunning, visibilityChangeHandler]);
+
+  //Handlers
+  const startHandler = () => {
+    setIsRunning(true);
+  };
+  const stopHandler = () => {
+    setIsRunning(false);
+    setElapsedTimeInSeconds(0);
+  };
+  const pauseHandler = () => {
+    setIsPaused(true);
+    setIsRunning(false);
+  };
+  const continueHandler = () => {
+    setIsRunning(true);
+    setIsPaused(false);
+  };
+
   return (
     <div className="app">
       <Nav />
       <Switch>
         <Route path="/" exact>
-          <Timer />
+          <Timer
+            timeInSeconds={pomodoroTimeInSeconds}
+            elapsedTimeInSeconds={elapsedTimeInSeconds}
+            isRunning={isRunning}
+            isPaused={isPaused}
+            onStart={startHandler}
+            onStop={stopHandler}
+            onPause={pauseHandler}
+            onContinue={continueHandler}
+          />
         </Route>
         <Route path="/tasks">
           <Tasks />
--- src/components/pages/Timer.js
+++ src/components/pages/Timer.js
@@ -1,92 +1,32 @@
-import { useState, useEffect, useContext, useCallback, useRef } from "react";
-
 import ProgressCircle from "../ProgressCircle";
 import ControlButtons from "../ControlButtons";
-import SettingsContext from "../context/settings-contex";
-import completedSound from "../../sounds/completed.mp3";
+
 import "./Timer.css";
 
-const Timer = () => {
-  const [isRunning, setIsRunning] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
-  const [elapsedTimeInSeconds, setElapsedTimeInSeconds] = useState(0);
-
-  const ctx = useContext(SettingsContext);
-  const { pomodoroMins, pomodoroSecs } = ctx.settings;
-  const pomodoroTimeInSeconds = pomodoroMins * 60 + parseInt(pomodoroSecs);
-
-  // Run the timer every 0.1s
-  useEffect(() => {
-    let interval;
-    if (elapsedTimeInSeconds >= pomodoroTimeInSeconds) {
-      const audio = new Audio(completedSound);
-      audio.play();
-      setIsRunning(false);
-      setElapsedTimeInSeconds(0);
-    }
-    isRunning
-      ? (interval = setInterval(() => {
-          setElapsedTimeInSeconds(prevTime => prevTime + 0.1);
-        }, 100))
-      : clearInterval(interval);
-    return () => clearInterval(interval);
-  }, [elapsedTimeInSeconds, pomodoroTimeInSeconds, isRunning]);
-
-  //Run the timer when tab is inactive
-  const hideTime = useRef(0);
-
-  const visibilityChangeHandler = useCallback(() => {
-    if (document.hidden) {
-      hideTime.current = Date.now();
-    } else {
-      const elapsedTimeWhenHidden = (Date.now() - hideTime.current) / 1000;
-
-      setElapsedTimeInSeconds(
-        prevElapsedTime => prevElapsedTime + elapsedTimeWhenHidden
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isRunning) {
-      document.addEventListener("visibilitychange", visibilityChangeHandler);
-    }
-    return () => {
-      document.removeEventListener("visibilitychange", visibilityChangeHandler);
-    };
-  }, [isRunning, visibilityChangeHandler]);
-
-  //Handlers
-  const startHandler = () => {
-    setIsRunning(true);
-  };
-  const stopHandler = () => {
-    setIsRunning(false);
-    setElapsedTimeInSeconds(0);
-  };
-  const pauseHandler = () => {
-    setIsPaused(true);
-    setIsRunning(false);
-  };
-  const continueHandler = () => {
-    setIsRunning(true);
-    setIsPaused(false);
-  };
-
+const Timer = ({
+  timeInSeconds,
+  elapsedTimeInSeconds,
+  isRunning,
+  isPaused,
+  onStart,
+  onStop,
+  onPause,
+  onContinue,
+}) => {
   return (
     <>
       <h1>Timer</h1>
       <ProgressCircle
-        timeInSeconds={pomodoroTimeInSeconds}
+        timeInSeconds={timeInSeconds}
         elapsedTimeInSeconds={elapsedTimeInSeconds}
       />
       <ControlButtons
         isRunning={isRunning}
         isPaused={isPaused}
-        onStart={startHandler}
-        onStop={stopHandler}
-        onPause={pauseHandler}
-        onContinue={continueHandler}
+        onStart={onStart}
+        onStop={onStop}
+        onPause={onPause}
+        onContinue={onContinue}
       />
     </>
   );
