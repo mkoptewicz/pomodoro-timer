e21836ee93d69e47e76d12f2c2af6e942da47988
Michal Koptewicz <mkoptewicz@gmail.com>
2021-09-13 15:18:25 +0200

    add basic styling to tasks section

---
+++ src/components/Task.js
@@ -0,0 +1,28 @@
+import { ReactComponent as Check } from "../icons/check.svg";
+import { ReactComponent as Play } from "../icons/play.svg";
+import { ReactComponent as Edit } from "../icons/edit.svg";
+import { ReactComponent as Delete } from "../icons/delete.svg";
+import "./Task.css";
+const Task = () => {
+  return (
+    <div className="task">
+      <button className="button-status">
+        <Check />
+      </button>
+      <h3>Task title but very long</h3>
+      <button className="button-timer">
+        <Play />
+      </button>
+      <button className="button-edit">
+        <Edit />
+      </button>
+      <button className="button-delete">
+        <Delete />
+      </button>
+      <p>25 min</p>
+      <div className="completed">0/4</div>
+    </div>
+  );
+};
+
+export default Task;
--- src/components/pages/Tasks.js
+++ src/components/pages/Tasks.js
@@ -1,5 +1,20 @@
+import Task from "../Task";
+import "./Tasks.css"
+
 const Tasks = () => {
- return ( <h2>Tasks</h2> );
-}
- 
+  return (
+    <>
+      <h2>Tasks</h2>
+      <h3>Active tasks</h3>
+      <div className="task-container">
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </div>
+      <h3>Completed tasks</h3>
+    </>
+  );
+};
+
 export default Tasks;
